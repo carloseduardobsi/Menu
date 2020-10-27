d872af54667cd00a83cc4540bee71d5a1659ccc7
Carlos Eduardo <carloseduardobsi@gmail.com>
2020-10-27 20:48:16 -0300

    Adicioneu a possibilidade de ocultar menus no grupo.

--- app/src/main/java/br/example/menu/MainActivity.java
+++ app/src/main/java/br/example/menu/MainActivity.java
@@ -9,15 +9,25 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    //aqui posso setar a visibilidade do grupo de itens do menu.
+   boolean visibilidade=false;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_menu, menu);
+
         return true;
     }
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.setGroupVisible(R.id.group_arquivo, visibilidade);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.novo:
                 startActivity(new Intent(this, SegundaActivity.class));
@@ -25,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
             case R.id.sair:
                 finish();
                 return true;
+
+
+
             default:
                 return super.onOptionsItemSelected(item);
         }
